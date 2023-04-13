2de585413cf4fd9e29809ee5df7bbfb3431479de
prushton2 <github@prushton.com>
2023-04-13 15:03:52 -0400

    convert to tabs

--- src/endpoints/services.js
+++ src/endpoints/services.js
@@ -6,60 +6,60 @@ const servicesRouter = express.Router();
 module.exports = servicesRouter;
 
 servicesRouter.post("/create", async(req, res) => {
-    if(!auth.verifySession(req, res, "permissions.services.create")) {
-        return;
-    }
+	if(!auth.verifySession(req, res, "permissions.services.create")) {
+		return;
+	}
 
-    db.Services.load();
-    let highest;
-    for(let i in db.Services.table) {
-        highest = i;
-    }
-    highest = (parseInt(highest) + 1).toString();
+	db.Services.load();
+	let highest;
+	for(let i in db.Services.table) {
+		highest = i;
+	}
+	highest = (parseInt(highest) + 1).toString();
 
-    db.Services.create(highest, req.body.JSON);
-    db.Services.save();
+	db.Services.create(highest, req.body.JSON);
+	db.Services.save();
 
-    res.status(200);
-    res.send({"response": "Service Created"});
+	res.status(200);
+	res.send({"response": "Service Created"});
 });
 
 servicesRouter.patch("/modify", async(req, res) => {
-    if(!auth.verifySession(req, res, "permissions.services.modify")) {
-        return;
-    }
+	if(!auth.verifySession(req, res, "permissions.services.modify")) {
+		return;
+	}
 
-    db.Services.load();
-    db.Services.set(req.body.id, req.body.newJSON);
-    db.Services.save();
+	db.Services.load();
+	db.Services.set(req.body.id, req.body.newJSON);
+	db.Services.save();
 
-    res.status(200);
-    res.send({"response": "Service Modified"});
+	res.status(200);
+	res.send({"response": "Service Modified"});
 });
 
 servicesRouter.post("/delete", async(req, res) => {
-    if(!auth.verifySession(req, res, "permissions.services.delete")) {
-        return;
-    }
+	if(!auth.verifySession(req, res, "permissions.services.delete")) {
+		return;
+	}
 
-    db.Services.load()
-    db.Services.delete(req.body.id);
-    db.Services.save();
+	db.Services.load()
+	db.Services.delete(req.body.id);
+	db.Services.save();
 
-    res.status(200);
-    res.send({"response": "Service Deleted"});
+	res.status(200);
+	res.send({"response": "Service Deleted"});
 });
 
 servicesRouter.get("/getAll", async(req, res) => {
-    db.Services.load();
+	db.Services.load();
 
-    let services = [];
-    for(let i in db.Services.table) {
-        services.push({
-            ...db.Services.table[i], id: i
-        })
-    }
+	let services = [];
+	for(let i in db.Services.table) {
+		services.push({
+			...db.Services.table[i], id: i
+		})
+	}
 
-    res.status(200);
-    res.send({"response": services});
+	res.status(200);
+	res.send({"response": services});
 })
